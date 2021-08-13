cc291e60a429d9ce42c4c2ea655b1284c906d03b
Keating Reid <keating.reid@protonmail.com>
2021-08-13 12:40:35 -0400

    v2 post-feedback

--- gen_data_depot.py
+++ gen_data_depot.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 from collections import namedtuple
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import pandas as pd
+import urllib
 
 Element = namedtuple("Element", ["name", "url"])
+
+CATEGORIES = [
+    "About the Data Depot",
+    "Data Glossary",
+    "Community",
+    "Learning Resource",
+    "Project or Initiative",
+    "Working Group",
+    "Policy Instrument",
+    "Team",
+    "Organization",
+    "Working Document",
+    "Committee",
+]
 
 
 def df_to_elem(group):
@@ -14,33 +28,26 @@ def df_to_elem(group):
     return [make_elem(r) for _, r in group.iterrows()]
 
 
-# TODO: Split URL
-def load_data() -> dict:
-    df = (
-        pd.read_csv("entities_master.csv", usecols=["Entity Full Name", "Type", "URL"])
-        .dropna(subset=["Entity Full Name", "Type"])
-        .reset_index(drop=True)
-    )
-    df["Entity Name"] = df["Entity Full Name"].str.strip()
-    df.drop("Entity Full Name", axis=1, inplace=True)
-    df["URL"] = df["URL"].str.strip()
-    new_rows = {"Type": [], "URL": [], "Entity Name": []}
-    for i, row in df.iterrows():
-        types = [t.strip() for t in row["Type"].split(",")]
-        for extra_type in types[1:]:
-            new_rows["Type"].append(extra_type)
-            new_rows["URL"].append(row["URL"])
-            new_rows["Entity Name"].append(row["Entity Name"])
-        df.iloc[i]["Type"] = types[0]
-    df_long = pd.concat((df, pd.DataFrame.from_dict(new_rows))).reset_index(drop=True)
-    return {tpl[0]: df_to_elem(tpl[1]) for tpl in df_long.groupby("Type")}
+def format_link_text(item):
+    return item.replace("/", f"/{chr(0x200b)}")
+
+
+def gen_url(item):
+    if item != "About the Data Depot":
+        page_name = f"Data depot:{item}".capitalize()
+    else:
+        page_name = "Data depot"
+    return f"https://www.gcpedia.gc.ca/wiki/{urllib.parse.quote(page_name, safe='')}"
 
 
 def main():
     env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape())
+    env.globals.update(
+        format_link_text=format_link_text, gen_url=gen_url, CATEGORIES=CATEGORIES
+    )
     template = env.get_template("data_depot.j2")
     with open("data_depot_out.html", "w") as f:
-        f.write(template.render(data=load_data()))
+        f.write(template.render())
 
 
 if __name__ == "__main__":
